76a3cf169007cea3b0e6008e9746df2174b8aa6f
wushuo <2460896173@qq.com>
2018-06-13 18:52:40 +0800

    2018年6月13日 09:48:1

---
+++ Test1/中文编码.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+IDE:               PyCharm Community Edition
+-------------------------------------------------
+   Project Name:   PythonTest
+   File Name：     中文编码
+   Description :
+   Author :       Admin
+   date:          2018-6-6
+-------------------------------------------------
+   Change Activity:
+                   2018-6-6:
+-------------------------------------------------
+"""
+'''
+__author__ = 'Admin'
+print "你好世界"
+a, b = 0, 1
+while b < 100000:
+ print(b)
+ a, b = b, a+b
+
+'''
+#
+'''
+x=int(input("请输入数字"))
+if x>5:
+    print "大于五"
+elif x<5:
+    print "小于五"
+else:2**
+    print "等于五"
+'''
+
+File = open("text.txt" , "w")
+for i in range(1,3):
+    File.write("https://mall.jd.com/index-"+str(i)+".html" + "\n")
+    print("https://mall.jd.com/index-"+str(i)+".html" + "\n")
+File = open("text.txt" , "w")
+for i2 in range(5,9):
+    File.write("https://mall.jd.com/index-"+str(i2)+".html" + "\n")
+    print("https://mall.jd.com/index-"+str(i2)+".html" + "\n")
+
